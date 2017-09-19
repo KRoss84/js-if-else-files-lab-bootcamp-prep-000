e41aff69f244d67484afa37db1004ab8d602f30e
Kirby Ross <kmr02d@gmail.com>
2017-09-19 15:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var name = "Susan"
 var height = "74"
 
-if height === "74" {
+if name === "Susan" {
   console.log("This is correct")
 } else {
-  console.log("Expected to be 74")
+  console.log("Expected to be Susan")
 }
 
 // Don't worry about this
